8c88107981e419364e5d5e8c4474c00bbd35e479
suowik <pkiwols@gmail.com>
2012-12-08 18:03:18 +0100

    auth.

--- src/main/java/pl/edu/pk/ztbd/photogallery/dao/UserDAO.java
+++ src/main/java/pl/edu/pk/ztbd/photogallery/dao/UserDAO.java
@@ -1,6 +1,8 @@
 package pl.edu.pk.ztbd.photogallery.dao;
 
 import pl.edu.pk.ztbd.photogallery.db.ConnectionResolver;
+import pl.edu.pk.ztbd.photogallery.exceptions.UserNotFoundException;
+import pl.edu.pk.ztbd.photogallery.to.User;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -13,19 +15,8 @@ import java.sql.Statement;
  * Time: 13:48
  * To change this template use File | Settings | File Templates.
  */
-public class UserDAO {
-    public void tryMe(){
-        Connection connection = ConnectionResolver.getConnection();
-        try {
-            Statement statement = connection.createStatement();
-            statement.execute("INSERT INTO USERS(email,name,surname,password) VALUES ('1234','aa','bb','cc')");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        try {
-            connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+public interface UserDAO {
+    User login(String mail, String password) throws UserNotFoundException;
+    void register(User user);
+    void delete(User user);
 }
